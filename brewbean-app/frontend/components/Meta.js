ffd20b25da8893bef0dac06865fb682a48d7894b
candicecodes <ca.mayes239@gmail.com>
2020-04-13 01:10:01 -0400

    Added global styles

--- brewbean-app/frontend/components/Meta.js
+++ brewbean-app/frontend/components/Meta.js
@@ -5,6 +5,7 @@ const Meta = () => (
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         {/* <link rel="shortcut icon" href="/static/favicon.png" /> */}
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Jomolhari&display=swap" rel="stylesheet"></link>
         <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
         <title>Brewbean</title>
     </Head>    
